d81abd399fe8ef6edaa586d3c15be1a74c07df92
Erica Naglik <erica2605@live.com>
2018-10-19 11:01:02 -0700

    working map and locations

---
+++ User-Location/MapScreen.swift
@@ -0,0 +1,30 @@
+//
+//  MapScreen.swift
+//  User-Location
+//
+//  Created by Sean Allen on 8/24/18.
+//  Copyright © 2018 Sean Allen. All rights reserved.
+//
+
+import UIKit
+
+class MapScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
